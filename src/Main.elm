724fb17f94a2eeb8dc5a4321b4cd4355a3ef2d74
pfcoperez <contact@pablofranciscoperez.info>
2020-04-24 22:29:37 +0200

    Unfinished WS connection with finnhub: I can connect to it and get responses, now I need to parse and render this.

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
 import Html exposing (Html, table, tr, th, td, text, button, div, input)
@@ -13,6 +13,11 @@ main =
     , update = update
     , view = view
     }
+
+-- Low level WS ports
+
+port sendMessage : String -> Cmd msg
+port messageReceiver : (String -> msg) -> Sub msg
 
 -- MODEL
 
@@ -38,7 +43,8 @@ type alias State =
   { 
     symbolSelector : String,
     apiKey : Maybe String,
-    values : List Value
+    values : List Value,
+    wsLog: List String
   }
 
 init : () -> (State, Cmd Msg)
@@ -46,24 +52,45 @@ init () = (
   { symbolSelector = ""
   , apiKey = Nothing
   , values = []
-  }
-  , Cmd.none
+  , wsLog = [ "Init" ]
+  },
+  Cmd.none
   ) 
+
+-- Messages for WS port
+
+subscribeJson : String -> String
+subscribeJson symbol =
+  let
+    subscriptionJson = "{\"type\":\"subscribe\",\"symbol\":\"" ++ symbol ++ "\"}"
+  in subscriptionJson
 
 -- UPDATE
 
-type Msg = Subscribe | Unsubscribe String | SelectorChange String
+type Msg
+  = Subscribe
+  | Unsubscribe String
+  | SelectorChange String
+  | Receive String
+
+addWsLogEntry : List String -> String -> List String
+addWsLogEntry currentLog newEntry = newEntry :: List.take 9 currentLog
 
 update : Msg -> State -> (State, Cmd Msg)
 update msg model = case msg of
   SelectorChange newSelector -> ({ model | symbolSelector = newSelector }, Cmd.none)
-  Subscribe -> ({ model | symbolSelector = "", values = model.values ++ [ newValue model.symbolSelector ] }, Cmd.none)
+  Subscribe ->
+    let
+      subsCommand = sendMessage (subscribeJson model.symbolSelector)
+    in ({ model | symbolSelector = "", values = model.values ++ [ newValue model.symbolSelector ] }, subsCommand)
   Unsubscribe symbol -> ({ model | values = List.filter (\value -> value.symbol /= symbol) model.values }, Cmd.none)
+  -- Port messages
+  Receive value -> ({model | wsLog = addWsLogEntry model.wsLog value }, Cmd.none)
 
 -- SUBSCRIPTIONS
 
 subscriptions : State -> Sub Msg
-subscriptions _ = Sub.none
+subscriptions _ = Sub.batch [ messageReceiver Receive ]
 
 -- VIEW
 
@@ -110,9 +137,13 @@ addSymbolView currentSelector =
     button [ onClick Subscribe ] [ text "OK" ]
     ]
 
+logView : List String -> Html Msg
+logView entries = div [] (List.map text entries)
+
 view : State -> Html Msg
-view { values, symbolSelector } = 
+view { values, symbolSelector, wsLog } = 
   div [] [
     addSymbolView symbolSelector,
-    valuesTable values
+    valuesTable values,
+    logView wsLog
   ]
